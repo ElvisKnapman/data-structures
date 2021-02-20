19276221deaa1ec4255559944ed26bf41e11b3a8
Elvis Knapman <elvis@elvisknapman.com>
2021-02-20 15:17:14 -0500

    push method for pushing a new node to top of stack

--- Stacks/StackUsingLinkedList/Stack.js
+++ Stacks/StackUsingLinkedList/Stack.js
@@ -10,6 +10,31 @@ class Stack {
   peek() {
     return this.top;
   }
+
+  push(value) {
+    // if there is a valid value
+    if (value === undefined || value === null) {
+      throw new Error('Not a valid value. Was not pushed to the stack.');
+    }
+
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+      // if there's no nodes in the stack point the top and bottom properties to the new node
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      // reference current top node in the next property of new top node
+      newNode.next = this.top;
+
+      // change the top property to the new node
+      this.top = newNode;
+    }
+
+    // increase size of stack
+    this.length++;
+    return this;
+  }
 }
 
 module.exports = Stack;
