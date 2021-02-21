e866ec4f8a1f1cb019963aced8e29e0f9a749ccd
Elvis Knapman <elvis@elvisknapman.com>
2021-02-21 14:36:05 -0500

    peek method to get last item in stack

--- Stacks/StackUsingArray/Stack.js
+++ Stacks/StackUsingArray/Stack.js
@@ -2,6 +2,16 @@ class Stack {
   constructor() {
     this.data = [];
   }
+
+  peek() {
+    const length = this.data.length;
+    // if there are no items in the stack, return null
+    if (length === 0) {
+      return null;
+    }
+    // if there are items, return the last item on the stack
+    return this.data[length - 1];
+  }
 }
 
 module.exports = Stack;
