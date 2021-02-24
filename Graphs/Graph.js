e5474d0c3f8249b14759217d6e8070eb43423734
Elvis Knapman <elvis@elvisknapman.com>
2021-02-24 15:04:01 -0500

    addEdge() method to add connections between vertices

--- Graphs/Graph.js
+++ Graphs/Graph.js
@@ -8,6 +8,14 @@ class Graph {
     this.adjacentList[vertex] = [];
     this.vertices++;
   }
+
+  addEdge(vertex1, vertex2) {
+    // undirected graph
+
+    // edge connections go both ways
+    this.adjacentList[vertex1].push(vertex2);
+    this.adjacentList[vertex2].push(vertex1);
+  }
 }
 
 module.exports = Graph;
